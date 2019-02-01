5e27128002473b20a7c332d6a901b5164ca8d031
Andrew Allen <aa2630@mcla.edu>
2019-02-01 20:16:09 +0000

    I'll double check this before Wednesday but it seems to be working as intended.

--- server.js
+++ server.js
@@ -29,6 +29,56 @@ function requestHandler(req, res)
       res.write('<pre>'+sum+'</pre>');
       res.end('');
     }
+    else if(query['cmd'] == 'repeat')
+    {
+      console.log("Handling a request");
+      console.log(query);
+      var length = query['word'].length;
+      for (var i = 0; i < length; i++)
+      {
+        res.write('<pre>'+query['word']+'</pre>');
+      }
+      res.end('');
+      
+    }
+    else if(query['cmd'] == 'dotted')
+    {
+      console.log("Handling a request");
+      console.log(query);
+      var length1 = query['word1'].length;
+      var length2 = query['word2'].length;
+      var cLength = 30- length1 - length2;
+      res.write('<pre>'+query['word1']);
+      for(var i = 0; i < cLength; i++)
+      {
+        res.write(".");
+      }
+      res.write(query['word2']+'</pre>');
+      res.end('');
+    }
+    else if(query['cmd'] == 'stats')
+    {
+      console.log("Handling a request");
+      console.log(query);
+      var sum = 0;
+      var min = query['grades'][0];
+      var max = query['grades'][0];
+      for(var i in query['grades'])
+      {
+        sum = sum+parseInt(query['grades'][i]);
+        if(Math.min(query['grades'][i])>min)
+        {
+          min = Math.min(query['grades'][i]);
+        }
+        if(Math.max(query['grades'][i])<max)
+        {
+          max = Math.max(query['grades'][i]);
+        }
+      }
+      res.write('<pre> Ave: '+(sum/query['grades'].length)+' Min: '+min+' Max: '+max+'</pre>');
+      res.end('');
+      
+    }
     else
     {
       res.end('');
